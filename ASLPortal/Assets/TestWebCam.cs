aa0cb20f51c807c58b40e0aef09914a7c9e164fe
Nick Carpenetti <nickcarpenetti@gmail.com>
2018-04-12 22:36:30 -0700

    Webcam can display to a texture

---
+++ ASLPortal/Assets/TestWebCam.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestWebCam : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        WebCamTexture  webCamTexture = new WebCamTexture();
+        Renderer renderer = GetComponent<Renderer>();
+        renderer.material.mainTexture = webCamTexture;
+        webCamTexture.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
